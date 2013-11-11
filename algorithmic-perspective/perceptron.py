05fa087f851715e8c846891ede7f068a780b43a3
= <=>
2013-11-11 12:49:43 +0530

    First commit for the Machine Learning repo

---
+++ algorithmic-perspective/perceptron.py
@@ -0,0 +1,13 @@
+#####################################
+# Author:   Jivitesh Singh Dhaliwal
+# Date:     11-09-2013
+# Program:  Perceptron Learning Algo
+#####################################
+
+def __main__():
+    
+
+
+if __name__ == '__main__':
+    __main__()
+    exit(0)
